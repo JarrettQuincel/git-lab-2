1d9ea2ff68c17f59daa6527f0f09b9e840d978d8
Jarrett Quincel <jq064217@ohio.edu>
2022-05-10 19:11:44 -0400

    git-lab-program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Jarrett Quincel>
+ *        Date: <05/10/2022>
+ * Description: Hello Git program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
